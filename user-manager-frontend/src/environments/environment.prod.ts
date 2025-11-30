c68f35df0c8e4d609cd4ba1ea6e5ab2212eea005
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2025-11-30 13:13:02 -0300

    mock false

--- user-manager-frontend/src/environments/environment.prod.ts
+++ user-manager-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://user-manager-angular.onrender.com/api', 
   authApiUrl: 'https://user-manager-angular.onrender.com/api/auth/login',
-  useMockLogic: true
+  useMockLogic: false
 };
