5b63011986911ed17a0768c25963d00cfcdaddbe
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2025-11-30 12:54:52 -0300

    logica api true

--- user-manager-frontend/src/environments/environment.ts
+++ user-manager-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000', // URL do JSON Server
   authApiUrl: 'http://localhost:3000/users', // URL de "login" do Mock
-  useMockLogic: true
+  useMockLogic: false
 };
