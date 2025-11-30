59a05a82dd30d0540c3efd3276598fa5b4715ef8
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2025-11-30 13:17:26 -0300

    sem comentarios

--- user-manager-frontend/src/environments/environment.ts
+++ user-manager-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000', // URL do JSON Server
-  authApiUrl: 'http://localhost:3000/users', // URL de "login" do Mock
+  apiUrl: 'http://localhost:3000', 
+  authApiUrl: 'http://localhost:3000/users',
   useMockLogic: true
 };
