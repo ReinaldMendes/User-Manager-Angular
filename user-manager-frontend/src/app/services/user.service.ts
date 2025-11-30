f58569ac064883ee92ca599e922f249bfc476d80
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2025-11-30 14:05:07 -0300

    vercel.json

--- user-manager-frontend/src/app/services/user.service.ts
+++ user-manager-frontend/src/app/services/user.service.ts
@@ -14,26 +14,18 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  /**
-   * LOGIN
-   */
   authenticate(email: string, password: string): Observable<User | null> {
     if (environment.useMockLogic) {
-      // --- Lógica Mock ---
       return this.http.get<User[]>(this.api, { params: { email, password } }).pipe(
         map(arr => {
           const user = arr && arr.length ? arr[0] : null;
-          if (user) this.saveSession('mock-token', user); // Mock token
+          if (user) this.saveSession('mock-token', user);
           return user;
         })
       );
     } else {
-      // --- Lógica API Real ---
-      // POST para /api/auth/login
       return this.http.post<any>(environment.authApiUrl, { email, password }).pipe(
         map(response => {
-          // A API retorna { message, token, user: {...} }
-          // Salvamos o token e retornamos o usuário
           if (response.token && response.user) {
             this.saveSession(response.token, response.user);
             return response.user;
@@ -53,13 +45,7 @@ export class UserService {
     localStorage.removeItem('access_token');
     localStorage.removeItem('user_data');
   }
-
-  /**
-   * CRUD
-   */
   list(params?: any): Observable<User[]> {
-    // OBS: A sua API Node "getAllUsers" ATUALMENTE ignora filtros (params).
-    // Ela vai retornar tudo. O filtro terá que ser feito no front ou vc ajusta a API.
     return this.http.get<any[]>(this.api, { params }).pipe(
       map(users => users.map(u => this.normalizeId(u)))
     );
@@ -82,8 +68,6 @@ export class UserService {
   delete(id: string | number) {
     return this.http.delete(`${this.api}/${id}`);
   }
-
-  // Helper para lidar com _id (Mongo) vs id (Front/Mock)
   private normalizeId(user: any): User {
     if (user._id && !user.id) {
       user.id = user._id;
